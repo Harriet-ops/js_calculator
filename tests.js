9737c9f222dd7db18a740e5239ead36dd15682c1
HarrietAffoah Koranteng <hkoranteng757@gmail.com>
2025-05-18 06:24:10 +0000

    New commit

---
+++ tests.js
@@ -0,0 +1,44 @@
+function sayhi(){
+    alert("Hello, I was just saying hi...");
+}
+
+
+function add(){
+    var text1 = document.getElementById('input1').value; // get text from text input
+    var text2 = document.getElementById('input2').value;
+
+    console.log("text1", text1);
+    console.log("text2", text2);
+    
+    var output = parseFloat(text1) + parseFloat(text2)
+
+   document.getElementById('output').innerHTML = 'The answer is '+ output;
+}
+
+function subtract(){
+    var text1 = document.getElementById('input1').value; // get text from text input
+    var text2 = document.getElementById('input2').value;
+    
+    var output = parseFloat(text1) - parseFloat(text2)
+
+   document.getElementById('output').innerHTML = 'The answer is '+ output;
+}
+
+function multiply(){
+    var text1 = document.getElementById('input1').value; // get text from text input
+    var text2 = document.getElementById('input2').value;
+    
+    var output = parseFloat(text1) * parseFloat(text2)
+
+   document.getElementById('output').innerHTML = 'The answer is '+ output;
+}
+
+function division(){
+    var text1 = document.getElementById('input1').value; // get text from text input
+    var text2 = document.getElementById('input2').value;
+    
+    var output = parseFloat(text1) / parseFloat(text2)
+
+   document.getElementById('output').innerHTML = 'The answer is '+ output;
+}
+
